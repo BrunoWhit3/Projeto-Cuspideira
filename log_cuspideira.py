20fd563ac4d7cbaeda28af850605a4fc0ecb2ee2
BrunoWhit3 <127335147+BrunoWhit3@users.noreply.github.com>
2025-11-16 17:13:45 -0300

    Código do log do projeto em Python

---
+++ log_cuspideira.py
@@ -0,0 +1,35 @@
+# Troque "COM3" no Windows ou "/dev/ttyUSB0" / "/dev/ttyACM0" no Linux
+import serial
+import time
+
+def conectar_arduino(porta="/dev/ttyACM0", baud=9600):
+    while True:
+        try:
+            print(f"Tentando conectar ao Arduino em {porta}...")
+            arduino = serial.Serial(porta, baud, timeout=1)
+            print("Arduino conectado!")
+            return arduino
+        except:
+            print("Arduino não encontrado. Tentando novamente em 2s...")
+            time.sleep(2)
+
+def loop_principal():
+    arduino = conectar_arduino()
+
+    while True:
+        try:
+            if arduino.in_waiting > 0:
+                linha = arduino.readline().decode(errors="ignore").strip()
+                print("Arduino ->", linha)
+
+        except serial.SerialException:
+            print("⚠️ Arduino foi desconectado!")
+            print("Aguardando reconexão...")
+            arduino = conectar_arduino()
+
+        except Exception as e:
+            print("Erro inesperado:", e)
+            time.sleep(1)
+
+if __name__ == "__main__":
+    loop_principal()
